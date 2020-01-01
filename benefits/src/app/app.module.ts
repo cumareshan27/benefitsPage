10d4b7f73541d49ebde564cc5c4360eb25885374
cumareshan27 <cumareshan@yahoo.com>
2020-01-01 18:55:35 -0500

    initial blanket push

--- benefits/src/app/app.module.ts
+++ benefits/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFontAwesomeModule } from 'angular-font-awesome'
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeaderComponent } from './components/header/header.component';
